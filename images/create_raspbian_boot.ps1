6311c3131675fd43791b7829255b9dc0a2f46d57
Skip Sopscak <dsopscak@gmail.com>
2023-03-29 11:07:51 -0500

    Update documentary comments and add some info output to image creation powershell script

--- images/create_raspbian_boot.ps1
+++ images/create_raspbian_boot.ps1
@@ -3,7 +3,9 @@
 # A command line windows utility for creating raspbian boot images with
 # ssh enabled. Must run from an admin powershell console:
 #
-#   create_raspbian_boot.ps1 -ifile <image file> -device <drive letter> [-wpa <wpa_supplicant.conf>]
+#   create_raspbian_boot.ps1 -ifile <image file> -device <drive letter> [-wpa <wpa_supplicant.conf> -usr <userconf.txt>]
+#
+# https://www.raspberrypi.com/documentation/computers/configuration.html#setting-up-a-headless-raspberry-pi
 #
 # Get CommandLineDiskImager from
 #
@@ -14,6 +16,10 @@
 #   https://downloads.raspberrypi.org/raspbian_full_latest
 #   https://downloads.raspberrypi.org/raspbian_latest
 #   https://downloads.raspberrypi.org/raspbian_lite_latest
+#
+#   The above are "classic". Newer stuff:
+#   https://downloads.raspberrypi.org/raspios_lite_arm64
+#   https://downloads.raspberrypi.org/raspios_arm64/
 
 param (
   [Parameter(Mandatory=$true)][string]$ifile,
@@ -25,9 +31,11 @@ param (
 C:\CommandLineDiskImager\CommandLineDiskImager.exe $ifile $device
 sleep 10
 if ($PSBoundParameters.ContainsKey('wpa')) {
+    echo "" "copy $wpa '${device}:\wpa_supplicant.conf'"
     copy $wpa "${device}:\wpa_supplicant.conf"
 }
 if ($PSBoundParameters.ContainsKey('usr')) {
+  echo "" "copy $usr '${device}:\userconf.txt'"
   copy $usr "${device}:\userconf.txt"
 }
 echo "" >>"${device}:\ssh"
